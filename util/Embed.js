4c6a8dc9425f3c4c473eaa977d9f2ec4b5b6b3d5
Pyan <Pyan4444@gmail.com>
2020-08-26 20:36:44 -0400

    Removed timestamp from embed

--- util/Embed.js
+++ util/Embed.js
@@ -8,7 +8,6 @@ Embed.Create = function(title, desc, fields) {
         .setTitle(title)
         .setDescription(desc)
         .setAuthor('Fun Bot', 'https://raw.githubusercontent.com/PY44N/FunBot/master/img/Icon.png', 'https://github.com/PY44N/FunBot')
-        .setTimestamp()
         .setFooter('Fun Bot by Pyan and Green726');
         var i;
         for (i = 0; i < fields.length; i++) {
